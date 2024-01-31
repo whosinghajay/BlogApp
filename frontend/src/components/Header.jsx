0160a3861dbbf7596951112365c3169e7e890d7c
Ajay Singh <whosinghajay@gmail.com>
2024-01-31 08:39:18 +0530

    server to get started created

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -32,23 +32,23 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to={"/sign-in"}>
-          <Button gradientDuoTone={"purpleToBlue"}>
-            <Link to={"/sign-up"}>SignUp</Link>
+          <Button gradientDuoTone={"purpleToBlue"} outline>
+            SignIn
           </Button>
         </Link>
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
       </div>
-        <Navbar.Collapse>
-          <Navbar.Link active={path==="/"} as={"div"}>
-            <Link to={"/"}>Home</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==="/about"} as={"div"}>
-            <Link to={"/about"}>About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==="/projects"} as={"div"}>
-            <Link to={"/projects"}>Projects</Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to={"/"}>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to={"/about"}>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to={"/projects"}>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
